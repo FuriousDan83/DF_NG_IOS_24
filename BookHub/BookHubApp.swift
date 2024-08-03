b58b841479be0a10cd591547dec800eeabc6f784
Daniel Fourie <retief.dfourie@gmail.com>
2024-08-03 19:40:23 +0100

    Initial Commit

---
+++ BookHub/BookHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookHubApp.swift
+//  BookHub
+//
+//  Created by Daniel Fourie on 03/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
